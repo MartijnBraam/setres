0c2085b484dfde1ec0cb11718c6d3d53b2ed0f35
Martijn Braam <martijn@brixit.nl>
2016-09-09 10:44:23 +0200

    Added detection of ports

--- setres/__main__.py
+++ setres/__main__.py
@@ -13,11 +13,17 @@ def get_ports():
 if __name__ == '__main__':
     import argparse
 
+    ports = get_ports()
+    primary = None
+    port_list = []
+    for port in ports:
+        port_list.append(port[0])
+        if port[2] == "primary":
+            primary = port[0]
+
     parser = argparse.ArgumentParser(description="Xorg display resolution tool")
     parser.add_argument('width', type=int, help="Horisontal resolution")
     parser.add_argument('height', type=int, help="Vertical resolution")
     parser.add_argument('--fps', type=float, help='Refresh rate')
-    parser.add_argument('--port', type=str, help="Use this port if multiple exist")
+    parser.add_argument('--port', type=str, help="Use this port if multiple exist", default=primary, choices=port_list)
     args = parser.parse_args()
-
-    ports = get_ports()
